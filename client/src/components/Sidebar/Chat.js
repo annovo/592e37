4995e96720718738471e3df575e9dea5bbe0eaff
Anastasiia <ananas.nasya@gmail.com>
2022-03-23 14:22:15 -0700

    add ui for status feature

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { Box } from '@material-ui/core';
-import { BadgeAvatar, ChatContent } from '../Sidebar';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+import { BadgeAvatar, ChatContent } from "../Sidebar";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     borderRadius: 8,
     height: 80,
-    boxShadow: '0 2px 10px 0 rgba(88,133,196,0.05)',
+    boxShadow: "0 2px 10px 0 rgba(88,133,196,0.05)",
     marginBottom: 10,
-    display: 'flex',
-    alignItems: 'center',
-    '&:hover': {
-      cursor: 'grab',
+    display: "flex",
+    alignItems: "center",
+    "&:hover": {
+      cursor: "grab",
     },
+  },
+  bubble: {
+    backgroundColor: " #007FFF",
+    borderRadius: "46%",
+    height: 24,
+    marginRight: 17,
+  },
+  text: {
+    fontSize: 12,
+    color: "#FFFFFF",
+    padding: "3px 8px 0px 8px",
+    letterSpacing: -0.17,
   },
 }));
 
@@ -34,6 +46,9 @@ const Chat = ({ conversation, setActiveChat }) => {
         sidebar={true}
       />
       <ChatContent conversation={conversation} />
+      <Box className={classes.bubble}>
+        <Typography className={classes.text}>10</Typography>
+      </Box>
     </Box>
   );
 };
