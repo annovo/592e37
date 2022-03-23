7241d3e9b84b7cb55660ab39aae3bf9f2cce15b6
Anastasiia <ananas.nasya@gmail.com>
2022-03-23 14:22:15 -0700

    add ui for status feature

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import { Box } from '@material-ui/core';
-import { SenderBubble, OtherUserBubble } from '.';
-import moment from 'moment';
+import React from "react";
+import { Box } from "@material-ui/core";
+import { SenderBubble, OtherUserBubble } from ".";
+import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
   return (
     <Box>
-      {messages.map((message) => {
-        const time = moment(message.createdAt).format('h:mm');
+      {messages.map((message, index) => {
+        const time = moment(message.createdAt).format("h:mm");
+        const isLast = index === messages.length - 1;
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            isLast={isLast}
+            otherUser={isLast ? otherUser : null}
+          />
         ) : (
           <OtherUserBubble
             key={message.id}
